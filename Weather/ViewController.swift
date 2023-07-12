856b32fda4181e99325425b0096a22f4cf9a3f54
KaMaDi <artkamadi@gmail.com>
2023-07-12 23:45:51 +0300

    добавил серч бар

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        searchBar.delegate = self
     }
-
 
 }
 
+extension ViewController:UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //делаем запрос url 21:29
+    }
+}
